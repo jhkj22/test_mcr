8197dbba6154225382a33700bf0d6af593b6dc98
Your Name <you@example.com>
2018-01-09 16:43:23 +0900

    test

--- best.py
+++ best.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import pywt
 from bed import *
 
 img = cv2.imread('res/apple.jpg', 0)
@@ -42,6 +41,23 @@ for r in range(img.shape[0]):
     tmp = sorted(tmp, key=lambda t: t[-1][1])
 for o in tmp:
     store.append(o)
+store_h = sorted(store, key=lambda t: t[0][0])
+
+for n1, o1 in enumerate(store):
+    if n1 == 0:
+        print(o1)
+        print('')
+    for n2, o2 in enumerate(store_h):
+        if o1[-1][0] >= o2[0][0]:
+            continue
+        if len(o1) < 3 and len(o2) < 3:
+            continue
+        if n1 == 0:
+            print(o2)
+
+#for o in store_h:
+#    print(o)
+"""
 for ps in store:
     if len(ps) < 5:
         continue
@@ -53,7 +69,7 @@ plt.plot(points[1], points[0], 'bo', markersize=1)
 plt.imshow(img, cmap='gray', vmin=0, vmax=256)
 
 plt.show()
-
+"""
 
 
 
